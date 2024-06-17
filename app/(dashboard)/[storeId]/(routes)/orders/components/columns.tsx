31fb4a7eb84b11fcedd66d2786cfeeffeee9c817
Pana Dragos <pana749@gmail.com>
2024-06-17 22:59:39 +0300

    adaugari

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+export type OrderColumn = {
+  id: string;
+  phone: string;
+  address: string;
+  isPaid: boolean;
+  totalPrice: string;
+  editions: string;
+  createdAt: string;
+}
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "editions",
+    header: "Editions",
+  },
+  {
+    accessorKey: "phone",
+    header: "Phone",
+  },
+  {
+    accessorKey: "address",
+    header: "Address",
+  },
+  {
+    accessorKey: "totalPrice",
+    header: "Total price",
+  },
+  {
+    accessorKey: "isPaid",
+    header: "Paid",
+  },
+];
